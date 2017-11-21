9808be076a02ae29a61f6a6564ec25517345f933
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2017-11-21 15:30:13 -0800

    wip

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+import HelloWorld from './HelloWorld';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,8 +11,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h2 className="App-title">top kek</h2>
         </header>
+        <HelloWorld></HelloWorld>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/HelloWorld.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class HelloWorld extends Component {
+  render() {
+    return (
+      <div>
+        HelloWorld
+      </div>
+    );
+  }
+}
