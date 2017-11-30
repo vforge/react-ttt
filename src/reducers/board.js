5b1ed1b422697a7f1cbcafc579b5be4a5a707d89
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2017-11-29 17:39:42 -0800

    AI v0

--- src/reducers/board.js
+++ src/reducers/board.js
@@ -1,24 +1,14 @@
+import { getDumbMove } from '../utils/ai.js';
+
 const board = (state = [], action) => {
   switch (action.type) {
     case 'RESET_GAME': {
       return [];
     }
     case 'AI_MOVE': {
-      // @TODO AI WILL BE HERE, for now it's just random
-      let validIndices = '012345678'.split('');
-      
-      // filter alstready used indices
-      if (state.length) {
-        const usedIndices = state.map(item => item.index);
-        
-        validIndices = validIndices.filter(
-          item => usedIndices.indexOf(parseInt(item, 10)) === -1
-        );
-      }
-      
       const currentBoard = state.slice(0);
       currentBoard.push({
-        index: parseInt(validIndices[Math.floor(Math.random() * validIndices.length)], 10),
+        index: getDumbMove(currentBoard),
         player: action.player,
       });
       
--- src/utils/ai.js
+++ src/utils/ai.js
@@ -1,3 +1,15 @@
-export default function ai() {
+export function getDumbMove(currentBoardState) {
+  let validIndices = '012345678'.split('');
   
+  // filter alstready used indices
+  if (currentBoardState.length) {
+    const usedIndices = currentBoardState.map(item => item.index);
+    
+    validIndices = validIndices.filter(
+      item => usedIndices.indexOf(parseInt(item, 10)) === -1
+    );
+  }
+    
+  // return random valid index
+  return parseInt(validIndices[Math.floor(Math.random() * validIndices.length)], 10);
 }
